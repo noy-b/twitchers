f4ccbdb3a194f9752237cdf75c05cdff4de1d777
noy-b <fabr.pass@gmail.com>
2017-10-08 15:28:02 +0200

    Changed slide animation direction.

--- src/app/animations/slideupdown.animation.ts
+++ src/app/animations/slideupdown.animation.ts
@@ -6,10 +6,10 @@ export const slideUpDown =
       query(':leave nav', [
         style({ transform: 'translateY(0%)' }),
         animate('.3s 400ms ease-in',
-          style({ transform: 'translateY(-100%)' })),
+          style({ transform: 'translateY(100%)' })),
       ], { optional: true }),
       query(':enter nav', [
-        style({ transform: 'translateY(-100%)' }),
+        style({ transform: 'translateY(100%)' }),
         animate('.3s 400ms ease-out',
           style({ transform: 'translateY(0%)' })),
       ], { optional: true }),
