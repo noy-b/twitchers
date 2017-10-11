b64fbdc2c3338667077181a392b5992d3da89b49
noy-b <fabr.pass@gmail.com>
2017-10-11 11:30:36 +0200

    Minor code adjustments.

--- src/app/_animations/route-fade-in.animation.ts
+++ src/app/_animations/route-fade-in.animation.ts
@@ -4,20 +4,20 @@ const query = (s, a, o = { optional: true }) => q(s, a, o);
 // Root element animation
 export const routeFadeIn =
 trigger('routeFadeIn', [
-  transition(':enter', [
-      sequence([
-        style({ backgroundColor: 'rgba(0, 0, 0, 0)' }),
-        animate('.5s ease-in-out', style({ backgroundColor: 'rgba(0, 0, 0, .5)' })),
-        query('@routeSequenceAnim', [ animateChild() ])
-      ])
-    ]),
   transition(':leave', [
     sequence([
       query('@routeSequenceAnim', [ animateChild() ]),
       style({ backgroundColor: 'rgba(0, 0, 0, .5)' }),
       animate('.5s ease-in-out', style({ backgroundColor: 'rgba(0, 0, 0, 0)' })),
     ])
-  ])
+  ]),
+  transition(':enter', [
+      sequence([
+        style({ backgroundColor: 'rgba(0, 0, 0, 0)' }),
+        animate('.5s ease-in-out', style({ backgroundColor: 'rgba(0, 0, 0, .5)' })),
+        query('@routeSequenceAnim', [ animateChild() ])
+      ])
+    ])
 ]);
 // Children elements animation
 export const routeSequenceAnim =
@@ -29,22 +29,22 @@ trigger('routeSequenceAnim', [
         animate('.5s ease-out', style({ transform: 'translateY(100%)' }))
       ]),
       query('.streamer-header', [
-        style({ transform: 'translateX(0%)' }),
-        animate('.5s ease-out', style({ transform: 'translateX(100%)' }))
+        style({ transform: 'translateY(0%)' }),
+        animate('.3s ease-out', style({ transform: 'translateY(-100%)' }))
       ]),
     ])
   ]),
   transition(':enter', [
-    query('.streamer-header', [ style({ transform: 'translateX(-100%)' }) ]),
+    query('.streamer-header', [ style({ transform: 'translateY(-100%)' }) ]),
     query('.streamer-body', [ style({ transform: 'translateY(100%)' }) ]),
     sequence([
       query('.streamer-body', [
         style({ transform: 'translateY(100%)' }),
-        animate('.5s ease-in', style({ transform: 'translateY(0%)' }))
+        animate('.5s .5s ease-in', style({ transform: 'translateY(0%)' }))
       ]),
       query('.streamer-header', [
-        style({ transform: 'translateX(-100%)' }),
-        animate('.5s ease-in', style({ transform: 'translateX(0%)' }))
+        style({ transform: 'translateY(-100%)' }),
+        animate('.3s ease-in', style({ transform: 'translateY(0%)' }))
       ]),
     ])
   ])
