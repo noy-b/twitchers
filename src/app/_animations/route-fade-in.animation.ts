30de7bcd389949a88253e7b999470c64c01cfe28
noy-b <fabr.pass@gmail.com>
2017-10-20 12:51:53 +0200

    Slight animations adjustments.

--- src/app/_animations/route-fade-in.animation.ts
+++ src/app/_animations/route-fade-in.animation.ts
@@ -8,16 +8,16 @@ trigger('routeFadeIn', [
     sequence([
       query('@routeSequenceAnim', [ animateChild() ]),
       style({ backgroundColor: 'rgba(0, 0, 0, .5)' }),
-      animate('.5s ease-in-out', style({ backgroundColor: 'rgba(0, 0, 0, 0)' })),
+      animate('.3s ease-in-out', style({ backgroundColor: 'rgba(0, 0, 0, 0)' })),
     ])
   ]),
   transition(':enter', [
-      sequence([
-        style({ backgroundColor: 'rgba(0, 0, 0, 0)' }),
-        animate('.5s ease-in-out', style({ backgroundColor: 'rgba(0, 0, 0, .5)' })),
-        query('@routeSequenceAnim', [ animateChild() ])
-      ])
+    sequence([
+      style({ backgroundColor: 'rgba(0, 0, 0, 0)' }),
+      animate('.3s ease-in-out', style({ backgroundColor: 'rgba(0, 0, 0, .5)' })),
+      query('@routeSequenceAnim', [ animateChild() ])
     ])
+  ])
 ]);
 // Children elements animation
 export const routeSequenceAnim =
@@ -30,12 +30,12 @@ trigger('routeSequenceAnim', [
       ]),
       query('.streamer-header', [
         style({ transform: 'translateY(0%)' }),
-        animate('.3s ease-out', style({ transform: 'translateY(-100%)' }))
+        animate('.3s ease-out', style({ transform: 'translateY(100%)' }))
       ]),
     ])
   ]),
   transition(':enter', [
-    query('.streamer-header', [ style({ transform: 'translateY(-100%)' }) ]),
+    query('.streamer-header', [ style({ transform: 'translateY(100%)' }) ]),
     query('.streamer-body', [ style({ transform: 'translateY(100%)' }) ]),
     sequence([
       query('.streamer-body', [
@@ -43,7 +43,7 @@ trigger('routeSequenceAnim', [
         animate('.5s ease-in', style({ transform: 'translateY(0%)' }))
       ]),
       query('.streamer-header', [
-        style({ transform: 'translateY(-100%)' }),
+        style({ transform: 'translateY(100%)' }),
         animate('.3s ease-in', style({ transform: 'translateY(0%)' }))
       ]),
     ])
--- src/app/_animations/slideupdown.animation.ts
+++ src/app/_animations/slideupdown.animation.ts
@@ -1,17 +1,18 @@
-import { trigger, state, animate, transition, style, query } from '@angular/animations';
+import { trigger, state, animate, transition, style, query as q } from '@angular/animations';
+const query = (s, a, o = { optional: true }) => q(s, a, o);
 
 export const slideUpDown =
   trigger('slideUpDown', [
     transition('* => *', [
       query(':leave nav', [
-        style({ transform: 'translateY(0%)' }),
+        style({ transform: 'translateX(0%)' }),
         animate('.3s 400ms ease-in',
-          style({ transform: 'translateY(100%)' })),
-      ], { optional: true }),
+          style({ transform: 'translateX(-100%)' })),
+      ]),
       query(':enter nav', [
-        style({ transform: 'translateY(100%)' }),
+        style({ transform: 'translateX(-100%)' }),
         animate('.3s 400ms ease-out',
-          style({ transform: 'translateY(0%)' })),
-      ], { optional: true }),
+          style({ transform: 'translateX(0%)' })),
+      ]),
     ]),
   ]);
