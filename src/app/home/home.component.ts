529e9670045ba86e6ce71754bc575ea535957602
noy-b <fabr.pass@gmail.com>
2017-10-20 12:50:03 +0200

    Re-added title to homepage.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'tvrs-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  private title   = `Twitch/ers`;
+export class HomeComponent implements OnInit {
+
+  private title = `Twitch/ers`;
+
+  constructor(
+    private titleService: Title
+  ) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle(`Twitch/ers - Home`);
+  }
 }
